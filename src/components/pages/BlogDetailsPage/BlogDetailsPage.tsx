354bcbed79d2bee6f87ca4e59e5c1cac40746144
Isah Hamza <itshamzy@gmail.com>
2024-01-04 08:24:00 +0100

    integrate blog details page

--- src/components/pages/BlogDetailsPage/BlogDetailsPage.tsx
+++ src/components/pages/BlogDetailsPage/BlogDetailsPage.tsx
@@ -8,38 +8,42 @@ import { HiThumbUp } from "react-icons/hi";
 import { MdOutlineFacebook } from "react-icons/md";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BlogType } from "../BlogPage/BlogPage";
 
 const BlogDetailsPage = () => {
 
-  const router = useRouter()
+  const router = useRouter();
+  const [blog, setBlog] = useState({} as BlogType);
+  const blurDataURL = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7';
+
+  useEffect(() => {
+    axios.get('http://localhost/jps-blog-server/server.php?id=' + router.query.blogId).then((res) => {
+      setBlog(res.data.data);
+      console.log(res);
+    })
+  }, [])
 
   return (
     <div className="text-grey-2 pt-24 ">
       {/* @ts-ignore */}
       <div className="mt-10 max-w-[1200px] mx-auto pt-10 px-5">
-        <h3 className="font-semibold text-2xl sm:text-5xl text-center mb-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos quia ducimus!</h3>
-        <NextImage src={news} alt="news image" className="w-full sm:w-[500px] h-[400px]" />
+        <h3 className="font-semibold text-2xl sm:text-5xl text-center mb-10">{blog?.title}</h3>
+        <NextImage 
+        blurDataURL={blurDataURL}
+        src={blog?.image_url} alt="news image" className="w-full sm:w-[500px] h-[400px]" />
         <div className="pt-6 mt-10 flex items-center justify-between border-t">
           <div className="flex items-center gap-6 !text-xs">
-            <span>DECEMBER 18, 2023</span>
+            <span>{blog?.created_date}</span>
             |
-            <span>POSTED BY: <b>NORRIS ANDERSON</b></span>
+            <span>POSTED BY: <b>{blog?.author}</b></span>
             |
-            <span>CATEGORY: <b>PRIVATE INVESTIGATIONS</b></span>
+            <span>CATEGORY: <b>{blog?.category}</b></span>
           </div>
         </div>
-        <div className="mt-14">
-          Federal Trade Commission (FTC) issued an advisory warning consumers about scammers hiding malicious links within QR codes as an increasingly convenient means of accessing information. Scammers have taken advantage of this trend to steal sensitive personal data. QR codes have become an easy target for cybercriminals looking to breach individuals’ privacy. Their use may include creating malicious QR codes that, when scanned by users, lead them to fake websites or phishing pages designed to gather passwords, financial details, or any other sensitive data from them.
-          <br />  <br />
-          For your safety and to avoid becoming the target of QR code scams, it is imperative to exercise extreme caution and heed these important tips: Only scan QR codes from reliable and reputable sources; do not scan codes from unknown or dubious origins.
-          <br />  <br />
-          Make sure your QR code scanning app comes from a legitimate and reputable provider; some fraudulent apps may pose as legitimate scanners to trick users. Keep abreast of security alerts and advisories issued by authorities like the FTC to stay aware of potential threats and take preventative steps against them. Until you know a website’s legitimacy, avoid entering personal or sensitive data through QR codes.
-          <br />  <br />
-          For added protection against potential vulnerabilities and exploits, ensure that all security software and applications on your mobile device are regularly updated. Becoming informed and alert and taking precautionary steps against QR code scams are the keys to protecting themselves in today’s digital environment. The Federal Trade Commission emphasizes the importance of promptly reporting suspected fraudulent activities – this will contribute to combating cybercrime while safeguarding personal data. Stay vigilant, informed, and cautious for the best digital experience!
-          <br />  <br />
-          Federal Trade Commission (FTC) issued an advisory warning consumers about scammers hiding malicious links within QR codes as an increasingly convenient means of accessing information. Scammers have taken advantage of this trend to steal sensitive personal data. QR codes have become an easy target for cybercriminals looking to breach individuals’ privacy. Their use may include creating malicious QR codes that, when scanned by users, lead them to fake websites or phishing pages designed to gather passwords, financial details, or any other sensitive data from them.
-          <br />  <br />
-          For your safety and to avoid becoming the target of QR code scams, it is imperative to exercise extreme caution and heed these important tips: Only scan QR codes from reliable and reputable sources; do not scan codes from unknown or dubious origins.
+        <div className="my-20">
+          {blog?.body}
           <br />  <br />
           <p>
             Read More About. </p>
