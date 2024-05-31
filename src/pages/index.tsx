2bab8bcbb64104208dcade6d8cb357d341caf2d7
John Akande <johnakande@MacBook-Pro.local>
2024-05-31 11:23:57 +0100

    meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Layout from "@/components/templates/Layout/AppLayout";
 import Landing from "@/components/pages/LandingPage/Landing";
 import type { NextPage } from "next";
-
+import Head from "next/head";
 const Home: NextPage = () => {
+
   return (
+    <>
+   
     <Layout>
+    <Head>
+      <title>Joint Power Security Services INC</title>
+    </Head>
       <Landing />
     </Layout>
+    </>
   );
 };
 
